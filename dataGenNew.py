905f8440c5fbdf2a45d185300c97da900ea5af16
Tareq Jaouni <jaounitareq@yahoo.ca>
2023-06-30 18:03:14 -0400

    New generator object introduced. Added new neural network architectures to learn the new problem

---
+++ dataGenNew.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a04626e5563e2af8adb45baa3d9a4ad49681d282a048b22a10d6095f9e3a47a9
+size 6521
--- neuralnet.py
+++ neuralnet.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:165d29ed2e8d46efb6ab4f3eb3b830d3da060ed8d8fa10e889713d9798eb9735
-size 4970
+oid sha256:eb24b40a5243257a4ed6e64ad6084b75e08cd557e3009a393244f2c0f69c043c
+size 7141
