ab9fbe65ae3c1e18568be95df0277c40918689b3
Tareq Jaouni <jaounitareq@yahoo.ca>
2023-10-16 10:45:05 -0400

    Latest version of code with corrections discussed in meetings prior. datagenLocal.py is a script that you can use to generate local datasets.

---
+++ datagenLocal.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:69184cc37f445a4f7eb128f44405ad6242451577ebe5bceea9af257154c98798
+size 3798
--- neuralnet.py
+++ neuralnet.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f50c6aaaf46d01210630c786d434d57bdbc297e671faf3c03d79da300a9232c2
-size 12712
+oid sha256:096f86d3613851e053fa291284137cfd4ba3bc9b5a599c7715a4f1547b95d265
+size 12754
