1a002562fc2e5b5750ce1178f162ef856b7f0fad
Tareq Jaouni <jaounitareq@yahoo.ca>
2023-12-21 20:01:04 -0500

    Updated waveplate generation w/ latest parameterization'

--- dataGenNew.py
+++ dataGenNew.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5cdcb3443e86069848bf72e720725321f945ed8a0940f4a338348ea96c049a73
-size 27772
+oid sha256:ffb8cf3bbc4b4f6bfc9d7803b1842b119c65569c9641d67aedfb698892f6759b
+size 28273
--- datagenLocal.py
+++ datagenLocal.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0f9d7b3e74127ca1930ec2b3762700ec291c7ea8237b18f05d462608596990ec
-size 7018
+oid sha256:f12a3b79e269d5d487b8c90bf2713c4cd1da30f8081d7fe63b1bb7d40eb92d34
+size 7805
