96f40820c836d621e9b2242c688e965d800273a8
Tareq Jaouni <jaounitareq@yahoo.ca>
2023-12-25 14:27:25 -0500

    Removed cartesian functions. Added new function to handle normalization

--- dataGenNew.py
+++ dataGenNew.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ffb8cf3bbc4b4f6bfc9d7803b1842b119c65569c9641d67aedfb698892f6759b
-size 28273
+oid sha256:02ee83b3a0c68bd25f96ffc40b7318bb27c77730176b0fd98b33fced9562bc53
+size 28135
--- datagenLocal.py
+++ datagenLocal.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f12a3b79e269d5d487b8c90bf2713c4cd1da30f8081d7fe63b1bb7d40eb92d34
-size 7805
+oid sha256:fb104720da10e5e31162bb02fc8743485a27842081089d349e141b16ef7a96da
+size 10415
